92ac39c818a7f5fc9c4abb2389e4cc7103395fac
nishuiq <nishuiq@gmail.com>
2022-03-24 15:51:19 +0800

    Update

--- exp/t42.asm
+++ exp/t42.asm
@@ -1,12 +1,12 @@
 assume cs:code
 
 code segment
-    mov ax,cs
+    mov ax,cs      ;______ 代码补全
     mov ds,ax
     mov ax,0020h
     mov es,ax
     mov bx,0
-    mov cx,0017h
+    mov cx,0017h   ;______ 先随意置入一个数，再Debug进行反编译查看 mov ax, 4c00h 的地址
     s:
         mov al,[bx]
         mov es:[bx],al
